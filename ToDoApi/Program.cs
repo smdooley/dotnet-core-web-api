c049f66833571ff9c465b29f96be66605dd7499d
Sean Dooley <s.dooley@ctidigital.com>
2025-02-28 14:28:29 +0000

    Implement in-memory database and add ToDo item model

--- ToDoApi/Program.cs
+++ ToDoApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,12 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<ToDoContext>(opt =>
+    opt.UseInMemoryDatabase("ToDoList"));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUi(options =>
+    {
+        options.DocumentPath = "/openapi/v1.json";
+    });
 }
 
 app.UseHttpsRedirection();
