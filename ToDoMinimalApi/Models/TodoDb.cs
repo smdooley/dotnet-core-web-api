890416f85a3fef4c5c6a1653db2df916632e439f
Sean Dooley <s.dooley@ctidigital.com>
2025-03-05 14:35:17 +0000

    Add Todo model and in-memory database context with CRUD endpoints

---
+++ ToDoMinimalApi/Models/TodoDb.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoMinimalApi.Models
+{
+    class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options)
+            : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
