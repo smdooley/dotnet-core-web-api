e20136cb23ebcb8829985687e6ed4ad1ac563f4e
Sean Dooley <s.dooley@ctidigital.com>
2025-03-05 14:53:14 +0000

    Add TodoItemDTO for improved data transfer and update API methods to use DTO

--- ToDoMinimalApi/Models/Todo.cs
+++ ToDoMinimalApi/Models/Todo.cs
@@ -5,5 +5,8 @@ namespace ToDoMinimalApi.Models
         public int Id { get; set; }
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
+
+        // The secret field needs to be hidden from this app, but an administrative app could choose to expose it.
+        public string? Secret { get; set; }
     }
 }
